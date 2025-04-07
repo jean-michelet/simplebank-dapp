71f357deeb462611299025125665241954eb5e4b
jean <jean.antoine.michelet@gmail.com>
2025-04-07 11:10:03 +0200

    first commit

---
+++ src/utils/parseError.ts
@@ -0,0 +1,10 @@
+export function parseError(error: unknown, unknownErrorMessage = "Something went wrong"): string {
+    if (typeof error === 'string') return error
+    if (!(error instanceof Error)) return unknownErrorMessage
+
+    // ethers v6 "reason"
+    if ("reason" in error && typeof error.reason === 'string') return error.reason;
+  
+    return error.message
+  }
+  
